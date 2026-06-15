385c99ea8b5e70e485d81cc9c3180f7fa01317d1
les1212 <carlyreivan@gmail.com>
2026-06-15 18:43:29 -0500

    Update EjercicioLeerArchivo.java

--- codigos/EjerciciosPracticos/EjercicioLeerArchivo.java
+++ codigos/EjerciciosPracticos/EjercicioLeerArchivo.java
@@ -2,19 +2,19 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class EjercicioLeerArchivo {
-
     public static void main(String[] args) {
-
         String archivo = "datos.csv";
         try {
             BufferedReader br = new BufferedReader(new FileReader(archivo));
             String linea;
             while((linea = br.readLine()) != null) {
-                System.out.println(linea);
+                String[] datos = linea.split(",");
+                System.out.println("--------------------------");
+                System.out.println("Nombre: " + datos[0]);
+                System.out.println("Edad: " + datos[1]);
+                System.out.println("Ciudad: " + datos[2]);
             }
-
             br.close();
-
         } catch(Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
