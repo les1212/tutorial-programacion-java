4151efc443df6b940f262dd613902fb890695b35
les1212 <carlyreivan@gmail.com>
2026-06-16 01:09:35 -0500

    Update EjercicioMetodos.java

--- codigos/EjerciciosPracticos/EjercicioMetodos.java
+++ codigos/EjerciciosPracticos/EjercicioMetodos.java
@@ -1,14 +1,25 @@
-public class EjercicioMetodos {
+
+// Clase principal del ejercicio
+public class MetodoMayor {
+
+    // Método que recibe dos números y devuelve el mayor
     public static int mayor(int a, int b) {
+        // Si a es mayor que b, devuelve a
         if (a > b) {
             return a;
         } else {
+            // Si no, devuelve b
             return b;
         }
     }
 
+    // Método principal (punto de entrada del programa)
     public static void main(String[] args) {
+
+        // Llamamos al método mayor con los valores 15 y 8
         int resultado = mayor(15, 8);
-        System.out.println("El número mayor es: " + resultado);
+
+        // Mostramos el resultado en pantalla
+        System.out.println("El número mayor es: " + resultado);  // Imprime: El número mayor es: 15
     }
 }
